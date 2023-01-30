aba261d0b558fc1bf1c00ef443a4df032fd0f869
OxMarco <>
2023-01-30 15:04:39 +0000

    stub

---
+++ src/services/HarvestableService.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.17;
+
+abstract contract HarvestableService {
+    event HarvestWasTriggered(uint256 indexed poolID);
+
+    /// @dev mapping containing the rewards for each pool ID
+    struct HarvestsData {
+        uint256[] totalRewards;
+        uint256 totalStakedAmount;
+    }
+    mapping(uint256 => HarvestsData) public harvests;
+
+    function _computeShareOfRewards(uint256 allowance, uint256 poolID) internal view returns (uint256[] memory) {
+        HarvestsData memory data = harvests[poolID];
+        uint256[] memory amounts = new uint256[](data.totalRewards.length);
+
+        for (uint16 i = 0; i < data.totalRewards.length; i++) {
+            //totalRewards * collateral / allowance...
+        }
+    }
+
+    function collectRewards(bytes memory data) public virtual;
+}
