e01ca8c53cac5f6998a4539e53b66f344d07965d
lsqrl <lsqrl91@gmail.com>
2023-02-22 18:37:37 +0100

    reduced testClose cyclomatic complexity

--- test/services/BalancerService.test.sol
+++ test/services/BalancerService.test.sol
@@ -43,201 +43,22 @@ import { console2 } from "forge-std/console2.sol";
 /// @dev overrides (except first implementation of virtual functions)
 ///
 
-/*
-contract BalancerServiceWeightedDAIWETH is BaseIntegrationServiceTest {
-    using GeneralMath for uint256;
+// contract BalancerServiceWeightedOHMWETH is BalancerServiceWeightedDAIWETH {
+//     using GeneralMath for uint256;
 
-    BalancerService internal immutable service;
-    address internal constant balancerVault = 0xBA12222222228d8Ba445958a75a0704d566BF2C8;
-    // address internal constant auraBooster = 0x7818A1DA7BD1E64c199029E86Ba244a9798eEE10;
+//     // address internal constant auraPoolID = 2;
 
-    bytes32 internal balancerPoolID = 0x0b09dea16768f0799065c475be02919503cb2a3500020000000000000000001a;
-    address internal gauge = 0x4ca6AC0509E6381Ca7CD872a6cdC0Fbf00600Fa1;
-    address internal bal = 0xba100000625a3754423978a60c9317c58a424e3D;
-
-    // address internal constant auraPoolID = 2;
-
-    string internal constant rpcUrl = "MAINNET_RPC_URL";
-    uint256 internal constant blockNumber = 16448665;
-
-    constructor() BaseIntegrationServiceTest(rpcUrl, blockNumber) {
-        vm.startPrank(admin);
-        service = new BalancerService(address(manager), balancerVault, bal);
-        vm.stopPrank();
-        loanLength = 2;
-        loanTokens = new address[](loanLength);
-        collateralTokens = new address[](1);
-        loanTokens[0] = 0x6B175474E89094C44Da98b954EedeAC495271d0F; // dai
-        loanTokens[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // weth
-        whales[loanTokens[0]] = 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
-        whales[loanTokens[1]] = 0x2fEb1512183545f48f6b9C5b4EbfCaF49CfCa6F3;
-        collateralTokens[0] = 0x0b09deA16768f0799065C475bE02919503cB2a35; // Pool 60 WETH - 40 DAI
-        serviceAddress = address(service);
-    }
-
-    function setUp() public virtual override {
-        super.setUp();
-        vm.prank(admin);
-        service.addPool(collateralTokens[0], balancerPoolID, gauge);
-    }
-
-    function _calculateExpectedTokens(uint256 amount0, uint256 amount1) internal view {
-
-        (, uint256[] memory balances, ) = IBalancerVault(balancerVault).getPoolTokens(balancerPoolID);
-        uint256[] memory normalizedWeights = IBalancerPool(collateralTokens[0]).getNormalizedWeights();
-        uint256[] memory amountsIn = new uint256[](loanLength);
-        amountsIn[0] = amount0;
-        amountsIn[1] = amount1;
-        uint256 maxWeightTokenIndex;
-        for(uint256 i = 0; i < loanLength; i++) {
-            if(normalizedWeights[i] > normalizedWeights[maxWeightTokenIndex])
-                maxWeightTokenIndex = i;
-        }    
-        uint256[] memory dueProtocolFeeAmounts = new uint256[](loanLength);
-        
-        dueProtocolFeeAmounts[maxWeightTokenIndex] = WeightedMath._calcDueTokenProtocolSwapFeeAmount(
-            balances[maxWeightTokenIndex],
-            normalizedWeights[maxWeightTokenIndex],
-            previousInvariant,
-            currentInvariant,
-            protocolSwapFeePercentage
-        );
-        uint256 bptAmountOut = WeightedMath._calcBptOutGivenExactTokensIn(
-            balances,
-            normalizedWeights,
-            amountsIn,
-            IERC20(collateralTokens[0]).totalSupply(),
-            IBalancerPool(collateralTokens[0]).getSwapFeePercentage()
-        );
-    }
-
-    function testOpen(uint256 amount0, uint256 loan0, uint256 margin0, uint256 amount1, uint256 loan1, uint256 margin1)
-        public
-    {
-        IService.Order memory order = _openOrder2(
-            amount0,
-            loan0,
-            margin0,
-            amount1,
-            loan1,
-            margin1,
-            0,
-            block.timestamp,
-            ""
-        );        
-        
-        service.open(order);
-
-        (
-            ,
-            IService.Collateral[] memory collaterals,
-            ,
-            
-        ) = service.getAgreement(1);
-        console2.log("bptAmountOut", bptAmountOut);
-        console2.log("collaterals[0].amount", collaterals[0].amount);
-        assertTrue(collaterals[0].amount == bptAmountOut);
-    }
-
-    function testClose(
-        uint256 amount0,
-        uint256 loan0,
-        uint256 margin0,
-        uint256 amount1,
-        uint256 loan1,
-        uint256 margin1,
-        uint256 minAmountsOutDai,
-        uint256 minAmountsOutUsdc
-    ) public {
-        (, uint256[] memory totalBalances, ) = IBalancerVault(balancerVault).getPoolTokens(balancerPoolID);
-        // WARNING: this is necessary otherwise Balancer math library throws a SUB_OVERFLOW error
-        vm.assume(minAmountsOutDai <= totalBalances[0]);
-        vm.assume(minAmountsOutUsdc <= totalBalances[1]);
-
-        testOpen(amount0, loan0, margin0, amount1, loan1, margin1);
-
-        uint256[] memory minAmountsOut = new uint256[](2);
-        // Fees make the initial investment always at a loss
-        // In this test we allow any loss: quoter tests will make this more precise
-        minAmountsOut[0] = minAmountsOutDai;
-        minAmountsOut[1] = minAmountsOutUsdc;
-        bytes memory data = abi.encode(minAmountsOut);
-
-        (IService.Loan[] memory loans, IService.Collateral[] memory collaterals, , ) = service.getAgreement(1);
-        bool slippageEnforced = minAmountsOutDai > loans[0].amount && minAmountsOutUsdc > loans[1].amount;
-        if (slippageEnforced) {
-            uint256[] memory normalizedWeights = IBalancerPool(collateralTokens[0]).getNormalizedWeights();
-            (, totalBalances, ) = IBalancerVault(balancerVault).getPoolTokens(balancerPoolID);
-            uint256 swapFee = IBalancerPool(collateralTokens[0]).getSwapFeePercentage();
-            uint256 bptTotalSupply = IERC20(collateralTokens[0]).totalSupply();
-            uint256 bptAmountOut = WeightedMath._calcBptInGivenExactTokensOut(
-                totalBalances,
-                normalizedWeights,
-                minAmountsOut,
-                bptTotalSupply,
-                swapFee
-            );
-            if (bptAmountOut > collaterals[0].amount) {
-                // This case is when slippage is exceeded
-                vm.expectRevert(bytes4(keccak256(abi.encodePacked("SlippageError()"))));
-                service.close(0, data);
-            } else {
-                uint256 initialDaiBalance = IERC20(loanTokens[0]).balanceOf(address(service));
-                uint256 initialWethBalance = IERC20(loanTokens[1]).balanceOf(address(service));
-                service.close(0, data);
-                // collateral tokens have been burned
-                assertTrue(IERC20(collateralTokens[0]).totalSupply() == bptTotalSupply - collaterals[0].amount);
-                // min amounts out are respected
-                assertTrue(IERC20(loanTokens[0]).balanceOf(address(service)) >= initialDaiBalance + minAmountsOut[0]);
-                assertTrue(IERC20(loanTokens[1]).balanceOf(address(service)) >= initialWethBalance + minAmountsOut[1]);
-            }
-        } else {
-            service.close(0, data);
-        }
-    }
-
-    function testQuote(uint256 amount0, uint256 loan0, uint256 margin0, uint256 amount1, uint256 loan1, uint256 margin1)
-        public
-    {
-        testOpen(amount0, loan0, margin0, amount1, loan1, margin1);
-        (
-            IService.Loan[] memory loan,
-            IService.Collateral[] memory collateral,
-            uint256 createdAt,
-            IService.Status status
-        ) = service.getAgreement(1);
-
-        IService.Agreement memory agreement = IService.Agreement(loan, collateral, createdAt, status);
-
-        (uint256[] memory profits, ) = service.quote(agreement);
-    }
-
-    // testAddPool() public {
-
-    // }
-
-    // testRemovePool() public {
-
-    // }
-}
-
-contract BalancerServiceWeightedOHMWETH is BalancerServiceWeightedDAIWETH {
-    using GeneralMath for uint256;
-
-    // address internal constant auraPoolID = 2;
-
-    function setUp() public override {
-        loanTokens[0] = 0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5; // ohm
-        loanTokens[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // weth
-        whales[loanTokens[0]] = 0x64C0fe73Dff66a8b9b803A1796082a575899DD26;
-        whales[loanTokens[1]] = 0x2fEb1512183545f48f6b9C5b4EbfCaF49CfCa6F3;
-        collateralTokens[0] = 0xD1eC5e215E8148D76F4460e4097FD3d5ae0A3558; // Pool 50 OHM - 50 WETH
-        balancerPoolID = 0xd1ec5e215e8148d76f4460e4097fd3d5ae0a35580002000000000000000003d3;
-        gauge = address(0);
-        super.setUp();
-    }
-}
-*/
+//     function setUp() public override {
+//         loanTokens[0] = 0x64aa3364F17a4D01c6f1751Fd97C2BD3D7e7f1D5; // ohm
+//         loanTokens[1] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // weth
+//         whales[loanTokens[0]] = 0x64C0fe73Dff66a8b9b803A1796082a575899DD26;
+//         whales[loanTokens[1]] = 0x2fEb1512183545f48f6b9C5b4EbfCaF49CfCa6F3;
+//         collateralTokens[0] = 0xD1eC5e215E8148D76F4460e4097FD3d5ae0A3558; // Pool 50 OHM - 50 WETH
+//         balancerPoolID = 0xd1ec5e215e8148d76f4460e4097fd3d5ae0a35580002000000000000000003d3;
+//         gauge = address(0);
+//         super.setUp();
+//     }
+// }
 
 contract BalancerServiceWeightedTriPool is BaseIntegrationServiceTest {
     using GeneralMath for uint256;
@@ -449,13 +270,15 @@ contract BalancerServiceWeightedTriPool is BaseIntegrationServiceTest {
             service.close(0, data);
         } else {
             for (uint256 i = 0; i < loanLength; i++) {
-                if (minAmountsOut[i] < actualLoans[i].amount) minAmountsOut[i] = actualLoans[i].amount;
+                minAmountsOut[i] = GeneralMath.max(minAmountsOut[i], actualLoans[i].amount);
             }
 
             (, balances, ) = IBalancerVault(balancerVault).getPoolTokens(balancerPoolID);
             uint256 firstStep = _calculateExpectedBPTFromExit(balances, minAmountsOut);
             if (firstStep > collaterals[0].amount) {
+                // In this case we must annihilate minAmountsOut to obtain correct assertEq at the end
                 firstStep = 0;
+                minAmountsOut = new uint256[](loanLength);
             } else {
                 for (uint256 i = 0; i < loanLength; i++) {
                     balances[i] -= minAmountsOut[i];
@@ -471,13 +294,11 @@ contract BalancerServiceWeightedTriPool is BaseIntegrationServiceTest {
             assertEq(IERC20(collateralTokens[0]).totalSupply(), bptTotalSupply - collaterals[0].amount);
             // min amounts out are respected
             for (uint256 i = 0; i < loanLength; i++) {
-                // If firstStep > 0, minAmountsOut were obtained
-                if (firstStep > 0)
-                    assertEq(
-                        IERC20(loanTokens[i]).balanceOf(address(service)),
-                        initialBalances[i] + minAmountsOut[i] + finalAmounts[i]
-                    );
-                else assertEq(IERC20(loanTokens[i]).balanceOf(address(service)), initialBalances[i] + finalAmounts[i]);
+                // If firstStep = 0, minAmountsOut are also zero
+                assertEq(
+                    IERC20(loanTokens[i]).balanceOf(address(service)),
+                    initialBalances[i] + minAmountsOut[i] + finalAmounts[i]
+                );
             }
         }
     }
