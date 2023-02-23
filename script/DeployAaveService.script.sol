37dadccae5e563980b968545d07eaa84dd46c9b2
OxMarco <>
2023-02-23 19:52:01 +0100

    aave service deployer script

---
+++ script/DeployAaveService.script.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.17;
+
+import { Base } from "./Base.script.sol";
+import { AaveService } from "../src/services/debit/AaveService.sol";
+
+contract DeployAaveService is Base {
+    address internal manager = vm.envAddress("MANAGER");
+    address internal aave = vm.envAddress("AAVE");
+
+    AaveService internal service;
+
+    function _run() internal override {
+        service = new AaveService(manager, aave);
+        assert(address(service) != address(0));
+        assert(keccak256(abi.encodePacked(service.name())) == keccak256(abi.encodePacked("AaveService")));
+    }
+}
