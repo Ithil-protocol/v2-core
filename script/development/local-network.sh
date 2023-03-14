972873c2ed95c59716e9f0371e0505d95b79c7c3
valerio <valerio.coltre@gmail.com>
2023-03-14 18:08:13 +0100

    better start command for network

---
+++ script/development/local-network.sh
@@ -0,0 +1,18 @@
+#/bin/bash
+set -e
+
+if [[ " $@ " =~ " --help " ]]; then
+  echo "Usage: ./local-network.sh [--clean]"
+  echo "                                   "
+  echo "       --clean: clean up the local network state. starts network from scratch"
+  exit 0
+fi
+
+if [[ " $@ " =~ " --clean " ]]; then
+  rm devnetwork.state
+fi
+
+anvil \
+  -f https://arb-mainnet.g.alchemy.com/v2/it4Um4ecMPP87zNShCGV2GhoFJvxulF8 \
+  --chain-id 1337 \
+  --state devnetwork.state
