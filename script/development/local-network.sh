646d4b2eb466a49a27aad8de36bb3598aeff5c8a
valerio <valerio.coltre@gmail.com>
2023-03-14 19:48:01 +0100

    setup script

--- script/development/local-network.sh
+++ script/development/local-network.sh
@@ -5,7 +5,20 @@ if [[ " $@ " =~ " --help " ]]; then
   echo "Usage: ./local-network.sh [--clean]"
   echo "                                   "
   echo "       --clean: clean up the local network state. starts network from scratch"
+  echo "       --setup: setup the local network state. starts network from scratch and deploys contracts"
   exit 0
+fi
+
+if [[ " $@ " =~ " --setup " ]]; then
+  rm devnetwork.state || true
+  anvil \
+    -f https://arb-mainnet.g.alchemy.com/v2/it4Um4ecMPP87zNShCGV2GhoFJvxulF8 \
+    --fork-block-number 69410517 \
+    --state devnetwork.state &
+  NETWORK_PID=$!
+  sleep 5
+  source lending-page.sh
+  kill $NETWORK_PID
 fi
 
 if [[ " $@ " =~ " --clean " ]]; then
