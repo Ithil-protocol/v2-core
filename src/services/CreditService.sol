a14a02490492a1e349d937bd07f49867e3d43985
lsqrl <lsqrl91@gmail.com>
2023-04-07 16:49:41 +0200

    direct mint and burn in credit services

--- src/services/CreditService.sol
+++ src/services/CreditService.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.17;
 
 import { Service } from "./Service.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IVault } from "../interfaces/IVault.sol";
 import { GeneralMath } from "../libraries/GeneralMath.sol";
 
@@ -21,7 +22,17 @@ abstract contract CreditService is Service {
                 agreement.collaterals[index].itemType != ItemType.ERC20 ||
                 agreement.collaterals[index].token != vaultAddress
             ) revert InvalidInput();
+            // Transfer tokens to this
+            IERC20(agreement.loans[index].token).transferFrom(msg.sender, address(this), agreement.loans[index].amount);
+
+            // Deposit toekns to the relevant vault
+            if (
+                IERC20(agreement.loans[index].token).allowance(address(this), vaultAddress) <
+                agreement.loans[index].amount
+            ) IERC20(agreement.loans[index].token).approve(vaultAddress, type(uint256).max);
             uint256 shares = IVault(vaultAddress).deposit(agreement.loans[index].amount, address(this));
+
+            // Register obtained shares and update exposures
             agreement.collaterals[index].amount = shares;
             exposures[agreement.loans[index].token] += shares;
         }
@@ -39,4 +50,28 @@ abstract contract CreditService is Service {
             );
         }
     }
+
+    function mintShares(address token, uint256 maxAmountIn) external returns (uint256) {
+        uint256 sharesToMint = _canMint(token);
+        uint256 sharesMinted;
+        if (sharesToMint > 0) {
+            sharesMinted = manager.directMint(token, address(this), sharesToMint, exposures[token], maxAmountIn);
+            exposures[token] += sharesMinted;
+        }
+        return sharesMinted;
+    }
+
+    function burnShares(address token, uint256 maxAmountIn) external returns (uint256) {
+        uint256 sharesToBurn = _canBurn(token);
+        uint256 sharesBurnt;
+        if (sharesToBurn > 0) {
+            sharesBurnt = manager.directBurn(token, address(this), sharesToBurn, maxAmountIn);
+            exposures[token] = exposures[token].positiveSub(sharesBurnt);
+        }
+        return sharesBurnt;
+    }
+
+    function _canMint(address token) internal virtual returns (uint256) {}
+
+    function _canBurn(address token) internal virtual returns (uint256) {}
 }
