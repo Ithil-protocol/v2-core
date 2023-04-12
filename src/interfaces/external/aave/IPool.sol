37c07ec97ef3819277d4b541d2efad4ec7a9e43b
lsqrl <lsqrl91@gmail.com>
2023-04-12 18:45:12 +0200

    target test with borrow and supply and fixed collateral calculation

--- src/interfaces/external/aave/IPool.sol
+++ src/interfaces/external/aave/IPool.sol
@@ -21,7 +21,7 @@ interface IPool {
     function withdraw(address asset, uint256 amount, address to) external returns (uint256);
 
     /**
-     * @notice Supplies an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
+     * @notice DEPRECATED Supplies an `amount` of underlying asset into the reserve, receiving in return overlying aTokens.
      * - E.g. User supplies 100 USDC and gets in return 100 aUSDC
      * @dev Deprecated: Use the `supply` function instead
      * @param asset The address of the underlying asset to supply
@@ -33,4 +33,17 @@ interface IPool {
      *   0 if the action is executed directly by the user, without any middle-man
      */
     function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
+
+    /**
+     * @notice Newer version of deposit
+     * @dev Deprecated: Use the `supply` function instead
+     * @param asset The address of the underlying asset to supply
+     * @param amount The amount to be supplied
+     * @param onBehalfOf The address that will receive the aTokens, same as msg.sender if the user
+     *   wants to receive them on his own wallet, or a different address if the beneficiary of aTokens
+     *   is a different wallet
+     * @param referralCode Code used to register the integrator originating the operation, for potential rewards.
+     *   0 if the action is executed directly by the user, without any middle-man
+     */
+    function supply(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
 }
