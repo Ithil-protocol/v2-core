401733ccca80a0adc0783d7221cdfa227c00f661
colthreepv <valerio.coltre@gmail.com>
2023-05-22 16:42:41 +0200

    full deploy now includes AAVE service

--- scripts/all.deploy.ts
+++ scripts/all.deploy.ts
@@ -1,12 +1,16 @@
 import { writeFileSync } from 'fs'
 
-import { createVault, deployManager } from './contracts'
+import { aaveSetCapacity, aaveToggleWhitelist, createVault, deployAave, deployManager } from './contracts'
 import { tokens } from './tokens'
 import { type LendingToken } from './types'
+
+const AAVE_POOL_ON_ARBITRUM = '0x794a61358D6845594F94dc1DB02A252b5b4814aD'
 
 const main = async () => {
   const manager = await deployManager()
   console.log(`Manager contract deployed to ${manager.address}`)
+  const aaveService = await deployAave(manager, AAVE_POOL_ON_ARBITRUM)
+  console.log(`AaveService contract deployed to ${aaveService.address}`)
 
   const vaults = await Promise.all(
     tokens.map(async (token): Promise<LendingToken> => {
@@ -15,11 +19,21 @@ const main = async () => {
     }),
   )
 
-  writeFileSync('manager-address.json', JSON.stringify({ manager: manager.address }, null, 2))
+  await Promise.all(tokens.map(async (token) => await aaveSetCapacity(manager, aaveService, token.tokenAddress)))
+  console.log(`Set capacity to 10^18 for ${tokens.length} tokens`)
+  await aaveToggleWhitelist(aaveService, false)
+  console.log('Disabled whitelist on AAVE service')
+
+  const contracts = {
+    manager: manager.address,
+    aaveService: aaveService.address,
+  }
+
+  writeFileSync('contracts.json', JSON.stringify(contracts, null, 2))
   writeFileSync('vaults.json', JSON.stringify(vaults, null, 2))
 
   console.log(`Wrote file vaults.json containing ${vaults.length} vaults`)
-  console.log('Wrote file manager-address.json containing the manager address')
+  console.log('Wrote file contracts.json containing other deploy addresses')
 }
 
 // We recommend this pattern to be able to use async/await everywhere
--- scripts/contracts.ts
+++ scripts/contracts.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat'
 
-import { type Manager } from '../typechain-types'
+import { type AaveService, type Manager } from '../typechain-types'
 import { type Address } from './types'
 
 export const deployManager = async () => {
@@ -15,4 +15,29 @@ export const createVault = async (manager: Manager, token: Address) => {
   await manager.create(token)
   const address = await manager.vaults(token)
   return address as Address
+}
+
+export const deployAave = async (manager: Manager, aavePool: Address) => {
+  const AaveService = await ethers.getContractFactory('AaveService')
+  const oneMonth = 3600n * 24n * 30n // 30 days expressed in seconds
+  const aaveService = await AaveService.deploy(manager.address, aavePool, oneMonth)
+
+  await aaveService.deployed()
+  return aaveService
+}
+
+export const aaveSetCapacity = async (
+  manager: Manager,
+  aaveService: AaveService,
+  token: Address,
+  capacity: bigint = 10n ** 18n,
+) => {
+  await manager.setCap(aaveService.address, token, capacity)
+}
+
+export const aaveToggleWhitelist = async (aaveService: AaveService, value: boolean) => {
+  const isWhitelistEnabled = await aaveService.enabled()
+
+  if (isWhitelistEnabled === value) return
+  await aaveService.toggleWhitelistFlag()
 }
