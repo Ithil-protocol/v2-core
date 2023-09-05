65b69b9ea5bd4445ee3693e42264700ebd72f139
lsqrl <lsqrl91@gmail.com>
2023-09-05 16:37:22 +0200

    fixed yield scenario test

--- test/scenarios/GmxService.scenario.test.sol
+++ test/scenarios/GmxService.scenario.test.sol
@@ -8,14 +8,18 @@ import { IVault } from "../../src/interfaces/IVault.sol";
 import { IService } from "../../src/interfaces/IService.sol";
 import { GeneralMath } from "../helpers/GeneralMath.sol";
 import { IManager, Manager } from "../../src/Manager.sol";
-import { AaveService } from "../../src/services/debit/AaveService.sol";
+import { GmxService } from "../../src/services/debit/GmxService.sol";
 
-import { console2 } from "forge-std/console2.sol";
+// import { console2 } from "forge-std/console2.sol";
 
-contract AaveScenarioTest is Test, IERC721Receiver {
+interface IQuotable is IService {
+    function quote(Agreement memory agreement) external view returns (uint256[] memory);
+}
+
+contract GmxScenarioTest is Test, IERC721Receiver {
     using GeneralMath for uint256;
 
-    IService internal constant aaveService = IService(0xb18865C919ADd78862D412AD253536d4C1C178Db);
+    IQuotable internal constant gmxService = IQuotable(0x2B1050f9df5f210Ec121B92E23c96216DB966aa5);
     address internal constant admin = 0xabcdBC2EcB47642Ee8cf52fD7B88Fa42FBb69f98;
     IVault[] internal vaults;
     address[] internal loanTokens;
@@ -23,27 +27,27 @@ contract AaveScenarioTest is Test, IERC721Receiver {
     address[] internal collateralTokens;
 
     string internal constant rpcUrl = "ARBITRUM_RPC_URL";
-    uint256 internal constant blockNumber = 123788806;
+    uint256 internal constant blockNumber = 124506946;
 
     constructor() {
         uint256 forkId = vm.createFork(vm.envString(rpcUrl), blockNumber);
         vaults = new IVault[](1);
         loanTokens = new address[](1);
         collateralTokens = new address[](1);
-        loanTokens[0] = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8; // USDC
-        whales[loanTokens[0]] = 0x489ee077994B6658eAfA855C308275EAd8097C4A;
-        collateralTokens[0] = 0x625E7708f30cA75bfd92586e17077590C60eb4cD;
-        vaults[0] = IVault(0x6b416C9727fb248C3097b5c1D10c39a7EBDFf239);
+        loanTokens[0] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9; // USDT
+        whales[loanTokens[0]] = 0xB38e8c17e38363aF6EbdCb3dAE12e0243582891D;
+        collateralTokens[0] = 0x1aDDD80E6039594eE970E5872D247bf0414C8903;
+        vaults[0] = IVault(0x8b002cf7380403329627149aA3D730E633BF1D33);
         vm.selectFork(forkId);
         vm.prank(admin);
-        (bool success, ) = address(aaveService).call(abi.encodeWithSignature("toggleWhitelistFlag()"));
+        (bool success, ) = address(gmxService).call(abi.encodeWithSignature("toggleWhitelistFlag()"));
         require(success, "toggleWhitelistFlag failed");
     }
 
     function setUp() public virtual {
         vm.prank(whales[loanTokens[0]]);
         IERC20(loanTokens[0]).transfer(address(this), 1e10);
-        IERC20(loanTokens[0]).approve(address(aaveService), type(uint256).max);
+        IERC20(loanTokens[0]).approve(address(gmxService), type(uint256).max);
     }
 
     function onERC721Received(
@@ -56,30 +60,20 @@ contract AaveScenarioTest is Test, IERC721Receiver {
     }
 
     function testOpen() public {
-        uint256 margin = 1e9;
-        uint256 loan = 1e9;
-        uint256 collateral = ((margin + loan) * 99) / 100;
+        uint256 margin = 1e8;
+        uint256 loan = 1e8;
         IService.Loan[] memory loans = new IService.Loan[](1);
         IService.Collateral[] memory collaterals = new IService.Collateral[](1);
         loans[0] = IService.Loan(loanTokens[0], loan, margin, (5e16 * 2 ** 128) + 5e16);
-        collaterals[0] = IService.Collateral(IService.ItemType.ERC20, collateralTokens[0], 0, collateral);
+        collaterals[0] = IService.Collateral(IService.ItemType.ERC20, collateralTokens[0], 0, 0);
         IService.Agreement memory agreement = IService.Agreement(loans, collaterals, 0, IService.Status.OPEN);
+        uint256[] memory quoted = gmxService.quote(agreement);
+        collaterals[0] = IService.Collateral(IService.ItemType.ERC20, collateralTokens[0], 0, (quoted[0] * 99) / 100);
+        // console2.log("collaterals[0].amount", collaterals[0].amount);
+        agreement = IService.Agreement(loans, collaterals, 0, IService.Status.OPEN);
         IService.Order memory order = IService.Order(agreement, abi.encode(0));
 
-        // uint256 freeLiquidity = vaults[0].freeLiquidity();
-        // console2.log("freeLiquidity", freeLiquidity);
-        // (, bytes memory data) = address(aaveService).staticcall(
-        //     abi.encodeWithSignature(
-        //         "computeBaseRateAndSpread(address,uint256,uint256,uint256)",
-        //         loanTokens[0],
-        //         loan,
-        //         margin,
-        //         freeLiquidity
-        //     )
-        // );
-        // (uint256 base, uint256 spread) = abi.decode(data, (uint256, uint256));
-        // console2.log("base", base);
-        // console2.log("spread", spread);
-        aaveService.open(order);
+        ///@dev Activate this to test!
+        // gmxService.open(order);
     }
 }
---
+++ test/scenarios/SeniorFixedYield.scenario.test.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity =0.8.18;
+
+import { Test } from "forge-std/Test.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IERC721Receiver } from "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import { IVault } from "../../src/interfaces/IVault.sol";
+import { IService } from "../../src/interfaces/IService.sol";
+import { GeneralMath } from "../helpers/GeneralMath.sol";
+import { IManager, Manager } from "../../src/Manager.sol";
+import { SeniorFixedYieldService } from "../../src/services/credit/SeniorFixedYieldService.sol";
+
+import { console2 } from "forge-std/console2.sol";
+
+contract SFYScenarioTest is Test, IERC721Receiver {
+    using GeneralMath for uint256;
+
+    IService internal constant seniorFixedYieldService = IService(0x40A87286EF87e17a48a5b266F94c918A53289956);
+    address internal constant admin = 0xabcdBC2EcB47642Ee8cf52fD7B88Fa42FBb69f98;
+    IVault[] internal vaults;
+    address[] internal loanTokens;
+    address[] internal collateralTokens;
+
+    string internal constant rpcUrl = "ARBITRUM_RPC_URL";
+    uint256 internal constant blockNumber = 128267144;
+
+    constructor() {
+        uint256 forkId = vm.createFork(vm.envString(rpcUrl), blockNumber);
+        vaults = new IVault[](1);
+        loanTokens = new address[](1);
+        collateralTokens = new address[](1);
+        loanTokens[0] = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9; // USDC
+        collateralTokens[0] = 0x8b002cf7380403329627149aA3D730E633BF1D33;
+        vaults[0] = IVault(0x8b002cf7380403329627149aA3D730E633BF1D33);
+        vm.selectFork(forkId);
+        vm.prank(admin);
+    }
+
+    function onERC721Received(
+        address /*operator*/,
+        address /*from*/,
+        uint256 /*tokenId*/,
+        bytes calldata /*data*/
+    ) external returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+    function testClose() public {
+        vm.startPrank(admin);
+        vm.expectRevert("ERC20: transfer amount exceeds balance");
+        seniorFixedYieldService.close(0, abi.encode(0));
+        vm.stopPrank();
+    }
+}
